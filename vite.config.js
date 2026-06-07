df77a9b7becf1bb0a6efcce357688c143f7b9392
masashitakano917-maker <masashitakano917@gmail.com>
2026-06-07 13:03:11 +0900

    Fix vite.config.js output path

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  root: 'milz2-site',
+  root: 'milz2-site-detailed',
   publicDir: '../public',
   build: {
     outDir: '../dist',
